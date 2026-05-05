386506d976d273c6ec1620d90679e16323eed75b
Georgios Syros <syros.g@northeastern.edu>
2026-05-05 12:22:50 -0400

    fix unsafe tool database drop

--- agent_backend/tools/documents.py
+++ agent_backend/tools/documents.py
@@ -5,64 +5,67 @@ from datetime import datetime
 from agent_backend.tools.base import BaseTool
 
 
+_OWNER_FIELD = "_owner"
+_INTERNAL_FIELDS = (_OWNER_FIELD, "_id")
+
+
 class LocalDocumentsTool(BaseTool):
     def __init__(self, user_email: str):
         super().__init__("documents")
         self.client = MongoClient(self.mongo_uri)
         self.user_email = user_email
-    
-    def seed_data(self, data: List[dict]):
-        db = self.client.get_database(self.tool_name)
-        collection_self = db.get_collection(self.user_email)
 
-        for document in data: 
-            collection_self.insert_one(document)
+    def _collection(self):
+        return self._get_collection(self.client)
+
+    def _strip_internal(self, doc: dict) -> dict:
+        for field in _INTERNAL_FIELDS:
+            doc.pop(field, None)
+        return doc
+
+    def seed_data(self, data: List[dict]):
+        collection = self._collection()
+
+        for document in data:
+            collection.insert_one({**document, _OWNER_FIELD: self.user_email})
 
     def search_by_query(self, query: str, limit: int = None) -> List[dict]:
         """
         Retrieve documents from the database based on the query.
         """
-        db = self.client.get_database(self.tool_name)
-        collection = db.get_collection(self.user_email)
-        
-        # Get 'limit' most recent documents.
+        collection = self._collection()
+
+        owner_filter = {_OWNER_FIELD: self.user_email}
         if query == "":
-            documents = collection.find()
+            documents = collection.find(owner_filter)
         else:
-            # Search for documents where any field matches the query
             documents = collection.find({
+                **owner_filter,
                 "$or": [
                     {"time": {"$regex": query, "$options": "i"}},
                     {"filename": {"$regex": query, "$options": "i"}},
                     {"content": {"$regex": query, "$options": "i"}},
                 ]
             })
-        documents = documents.sort("time", -1)  # Sort by time in descending order
-        
-        # If limit is None, get all documents.
+        documents = documents.sort("time", -1)
+
         if limit is not None:
             documents = documents.limit(limit)
-        
-        # Convert to list of dictionaries
-        documents = list(documents)
 
-        # Remove objectid : we only need from/subject/body/time
-        for document in documents:
-            document.pop("_id", None)
-
+        documents = [self._strip_internal(d) for d in documents]
         return documents
 
     def create_document(self, filename: str, content: str) -> bool:
         """
             Write document to file
         """
-        db = self.client.get_database(self.tool_name)
-        collection_self = db.get_collection(self.user_email)
+        collection = self._collection()
 
         document = {
             "time": datetime.now(),
             "filename": filename,
-            "content": content
+            "content": content,
+            _OWNER_FIELD: self.user_email,
         }
-        collection_self.insert_one(document)
+        collection.insert_one(document)
         return True
